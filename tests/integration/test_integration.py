410648875831c1d424a6382c029becc54277fcfe
farahlolah <farahwael158@gmail.com>
2025-10-19 15:22:56 +0500

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,7 +1,6 @@
 import requests
 
 def test_metrics_endpoint():
-    # Use service name and internal container port
-    r = requests.get('http://app:8081/metrics', timeout=5)
+    r = requests.get('http://my-ci-cd-pipeline_app_1:8081/metrics', timeout=5)
     assert r.status_code == 200
     assert 'http_requests_total' in r.text
