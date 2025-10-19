4909371be8d65f939ccd9624b64bd7fbe2962d7d
farahlolah <farahwael158@gmail.com>
2025-10-19 06:05:22 +0500

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,6 +1,6 @@
 import requests
 
 def test_metrics_endpoint():
-    r = requests.get('http://my-ci-cd-pipeline_app_1/metrics', timeout=5)
+    r = requests.get('http://app:8080/metrics', timeout=5)
     assert r.status_code == 200
     assert 'http_requests_total' in r.text
