9cbaf62a9c0e03b660dd7b7d1245cfed587b6ed8
farahlolah <farahwael158@gmail.com>
2025-10-19 15:12:02 +0500

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_metrics_endpoint():
-    # Use Docker Compose service name and internal port
-    r = requests.get('http://app:8080/metrics', timeout=5)
+    # Use service name and internal container port
+    r = requests.get('http://app:8081/metrics', timeout=5)
     assert r.status_code == 200
     assert 'http_requests_total' in r.text
